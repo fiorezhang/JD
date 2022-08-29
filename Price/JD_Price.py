ff4bbcde0230f95ac75688525514247db5b1cea9
Fiore <fiorezhang@sina.com>
2022-08-29 10:26:04 +0800

    增加时间戳，从csv提取配置信息

--- Price/JD_Price.py
+++ Price/JD_Price.py
@@ -19,7 +19,8 @@ import argparse
 import time
 import sys
 
-COOKIE = "__jdu=1597106510361611076602; shshshfpa=e8c04075-2ed8-8c17-9a2e-5eb000215962-1597106511; shshshfpb=hYFEa8t4oszf5zty/aI7r1Q==; __jdv=76161171|direct|-|none|-|1638343313368; PCSYCityID=CN_310000_310100_310115; areaId=2; pinId=lSwRbMKd-74tBDbKmwsL1w; pin=FioreZhang; unick=FioreZhang; _tp=IcTvqSKCNvN8JKtp2uhsOg==; _pst=FioreZhang; ipLoc-djd=2-2830-51803-0; shshshfp=1a5e708b76fe26c8443f300d10b6b119; __jdc=122270672; TrackID=1texs4UqbtSbga2lScwdKjyLU1dxI-DB0NDp4q-ecux3z05dNnD-GBuEqiKGI76Yahe2TyZuA9haOhmfryZoVObMVU5Blw2OGuodJPOTZI55Rg3ppuZFIZRJp4zXhTK9Z; ceshi3.com=201; token=d0bf370435bc5fdd1c4f0060845b90ec,3,910290; __tk=16101e1ef5c5c67686e452e544f92431,3,910290; __jda=122270672.1597106510361611076602.1597106510.1638519922.1638522072.28; wlfstk_smdl=3wsa372k6pi0qybgr9d6jik4x36tuyey; thor=C415B3C186C7F2E97A4AA78C1DB6835F00E6CC47BD365631899C49AF02D279F7D1B6355EBCA57C5EB2D804361F59F9FB130C3B5BAFFC0E60CF3D3E188C6F8070506E5655721308965D80B714DA9E74A333E85A90096D24486526F62BE36DDAECDA26E9A7827F47C1C8C295F580CBB7D0C966573D7A7763355B538E7781804595D649B12AAB2A8FF5FEE1B50382879E8D; __jdb=122270672.4.1597106510361611076602|28.1638522072; shshshsID=bf5b86abc43adfacda49721ea63f70cd_2_1638522376662; 3AB9D23F7A4B3C9B=IABYFHSAZ5YE4JK67BOOHWXZM3E3Q6LHRDFAOMFFBNIQYMWUZOXRA33UAIVOJD5FAGMHAXRKCCL2HARJ4LAJGRSQHE" 
+#cookie需要timely更新，否则影响抓取comment等等功能（页面刷新，控制台-网络-请求标头-cookie）
+COOKIE = "__jdu=1597106510361611076602; shshshfpa=e8c04075-2ed8-8c17-9a2e-5eb000215962-1597106511; pinId=lSwRbMKd-74tBDbKmwsL1w; shshshfpb=hYFEa8t4oszf5zty/aI7r1Q==; __jdv=76161171|direct|-|none|-|1661733370418; areaId=2; PCSYCityID=CN_310000_310100_0; shshshfp=594ba833ebdab9d6da8fd55a42246383; jsavif=0; __jda=122270672.1597106510361611076602.1597106510.1661733370.1661736699.64; __jdc=122270672; jsavif=0; ip_cityCode=2817; ipLoc-djd=2-2817-51973-0; wlfstk_smdl=uxj1ama95l44k3iy0vuzux861ko3ksre; TrackID=1wwh5dQPUO3-Jbsv7ZQp-mwhJOa6xjtAp-tnFZwG6FpF5hPQ-r3I9S4SXj2Ya_pHPmQx7PdSiEHoTQZZ5ZBbyqavSPq2KgqFGelWysuN0-PeE3W36-tVqiGuXke-tiibd; thor=C415B3C186C7F2E97A4AA78C1DB6835F123CCE19BAEDBD11C09CB538253C92B5800AB7FBD76057178E0BB5983DA0B64443D8A41C00B37F0CEEC32C59159E9418239FAA77892C866A27D21B442C00D79E51EDA0A9C646F1FB52ECFEAA2446880B43D5B2D714AB46B458FABB5C2E43EB120D0AC5C9B849FB847BC57310072DE35B25A5E005DACEA42980DE890E2DA670A1; pin=FioreZhang; unick=FioreZhang; ceshi3.com=201; _tp=IcTvqSKCNvN8JKtp2uhsOg==; _pst=FioreZhang; token=0d7dff58c81d855eab7f01712cca54d7,3,923187; __tk=sgPgdEvwrUrDJaAxstYxlonwJSYvqDbcsorRqEvwsUb1JcMEdrY1ebnprgnDqEbFdtPBIoBg,3,923187; __jdb=122270672.10.1597106510361611076602|64.1661736699; shshshsID=2258eb014a57d0c24fe40a8ae3255634_7_1661736877825; 3AB9D23F7A4B3C9B=IABYFHSAZ5YE4JK67BOOHWXZM3E3Q6LHRDFAOMFFBNIQYMWUZOXRA33UAIVOJD5FAGMHAXRKCCL2HARJ4LAJGRSQHE"
 
 TIMEOUT = 2
 RETRY = 5
@@ -88,9 +89,19 @@ def getList(keyword, countnum):
     skulist = []
     for page in range(1, MAXPAGE):
         print("Getting sku list for Page "+str(page))
-        skulist += getListPageUp(keyword, page)
-        skulist += getListPageDown(keyword, page)
-        skulist = list(set(skulist)) #去重
+        #获得每页上半页，如果没有新的sku就跳出循环
+        skulistUpdate = getListPageUp(keyword, page)
+        if len(skulistUpdate) == 0:
+            break
+        skulist += skulistUpdate
+        #获得每页下半页，如果没有新的sku就跳出循环
+        skulistUpdate = getListPageDown(keyword, page)
+        if len(skulistUpdate) == 0:
+            break
+        skulist += skulistUpdate
+        #去重
+        skulist = list(set(skulist)) 
+        #如果超过希望抓取的sku数量就跳出循环
         if len(skulist) >= countnum:
             break
         time.sleep(3)   #慢一点……
@@ -191,6 +202,8 @@ def getInfo(sku):
     listshop = []
     listbrand = []
     listinfo = []
+    listtableindex = []
+    listtabledata = []
     retry = RETRY
     while retry > 0:
         try:
@@ -202,6 +215,8 @@ def getInfo(sku):
             listshop = htmltree.xpath('//div[@class="J-hove-wrap EDropdown fr"]/div/div/a/text()')
             listbrand = htmltree.xpath('//ul[@id="parameter-brand"]/li/@title')
             listinfo = htmltree.xpath('//ul[@class="parameter2 p-parameter-list"]/li/text()')
+            listtableindex = htmltree.xpath('//div[@class="Ptable"]/div/dl/dl/dt/text()')
+            listtabledata = htmltree.xpath('//div[@class="Ptable"]/div/dl/dl/dd[not(contains(@class,"Ptable-tips"))]/text()')
         except Exception as ex:
             print(ex)
             pass
@@ -213,11 +228,14 @@ def getInfo(sku):
         print("== Retry getInfo(), can fix occational network lag")
         time.sleep(1)
     
+    #处理名字
     skuname = getSkuname(listskuname)
-    if listshop != [] and listhshop != None:
+    #处理店铺信息
+    if listshop != [] and listshop != None:
         shop = listshop[0]
     else:
         shop = ''
+    #处理品牌信息    
     if listbrand != [] and listbrand != None:
         brand = listbrand[0]
     else:
@@ -227,7 +245,9 @@ def getInfo(sku):
     print(shop)
     print(brand)
     print(listinfo)
-    return skuname, shop, brand, listinfo
+    print(listtableindex)
+    print(listtabledata)
+    return skuname, shop, brand, listinfo, listtableindex, listtabledata
 
 #剥离skuname并去除无用字符
 def getSkuname(listSkuname):
@@ -250,6 +270,15 @@ def appendCsv(file, row):
         write.writerow(row)
         f.close
 
+#读出CSV文件
+def parseCsv(file):
+    rows = []
+    with open(file, mode='r') as f:
+        read=csv.reader(f)
+        rows = [row for row in read]
+        f.close
+    return rows
+
 # ======== MISC ========
 #定义参数列表
 def getArgs():
@@ -259,7 +288,7 @@ def getArgs():
     args = parser.parse_args()
     return args
 
-KeywordList = ["游戏本", "笔记本", "轻薄本", "全能本", "创意本"]
+KeywordList = ["游戏本", "轻薄本", "设计本", "显卡", "EVO笔记本", "NVIDIA Studio创作本"]
 
 # ======== MAIN ========
 if __name__ == "__main__":
@@ -272,8 +301,22 @@ if __name__ == "__main__":
     
     #getInfo(100025263673)
     #while(True):
-    #    pass
+     #   pass
     
+    #从CSV表格检索当前关键字的配置，包括“商品介绍”“更多参数”（CSV需以逗号分隔元素）
+    listIndex = []
+    listDetail = []
+    rowsList = parseCsv("config.csv")
+    #print(rowsList)
+    for row in rowsList:
+        if row[0] == "商品介绍" and row[1] == KEYWORD: 
+            listIndex = row[2:]
+            print(listIndex)
+        if row[0] == "更多参数" and row[1] == KEYWORD:
+            listDetail = row[2:]
+            print(listDetail)
+    
+    '''
     #取得待抓取信息列表，要求同目录下有相关配置文件例如“游戏本.txt”“显卡.txt”
     listIndex = []
     with open(KEYWORD+".txt", 'r', encoding='utf-8') as f:
@@ -282,7 +325,16 @@ if __name__ == "__main__":
         listIndex = [x.strip() for x in listIndex if x.strip != '']
     print(listIndex)
     
-    fData ="DATA_"+str(SEARCH)+"_"+time.strftime("%Y_%m", time.localtime())+".csv"
+    #取得待抓取信息列表，要求同目录下有相关配置文件例如“游戏本.ini”“显卡.ini”
+    listDetail = []
+    with open(KEYWORD+".ini", 'r', encoding='utf-8') as f:
+        listDetail = list(f)
+        f.close()
+        listDetail = [x.strip() for x in listDetail if x.strip != '']
+    print(listDetail)    
+    '''
+    
+    fData ="DATA_"+str(KEYWORD)+"_"+time.strftime("%Y_%m", time.localtime())+".csv"
     
     fDataExist = False
     listskuOld = []
@@ -302,9 +354,12 @@ if __name__ == "__main__":
     
         #写第一行，如果是追加已经存在的同名文件则跳过
         if fDataExist == False:
-            row = ["商品全名", "价格", "店铺", "品牌"]
+            row = ["年", "月", "日", "星期", "时间"]
+            row.extend(["商品全名", "价格", "店铺", "品牌"])
             for index in listIndex:
                 row.append(index)
+            for index in listDetail:
+                row.append(index)    
             row.extend(["好评率", "中评率", "差评率", "好评数", "中评数", "差评数"])
             for i in range(NUMTAGS):
                 row.append("标签")
@@ -317,14 +372,16 @@ if __name__ == "__main__":
             print("Getting Price and Information for Sku "+str(sku))
             price = getPrice(sku)
            
-            skuname, shop, brand, listinfo = getInfo(sku)
+            skuname, shop, brand, listinfo, listtableindex, listtabledata = getInfo(sku)
             summary, tags = getComment(sku)
             
             if price == None or price == "" or skuname == None or skuname == "" or listinfo == None or len(listinfo) == 0 or summary[0] == 0:
                 time.sleep(3)
                 continue    #信息不全，跳过这一条不写入
             
-            row = [skuname, price, shop, brand]
+            #信息准备好后开始一次性写入一行
+            row = [time.strftime("%Y", time.localtime()), time.strftime("%m", time.localtime()), time.strftime("%d", time.localtime()), time.strftime("%A", time.localtime()), time.strftime("%H:%M", time.localtime())]
+            row.extend([skuname, price, shop, brand])
             listinfoindex = [x.split("：")[0] for x in listinfo]
             listinfodata = [x.split("：")[1] for x in listinfo]
             for index in listIndex:
@@ -332,6 +389,11 @@ if __name__ == "__main__":
                     row.append(listinfodata[listinfoindex.index(index)])
                 else:
                     row.append("")
+            for index in listDetail:
+                if index in listtableindex:
+                    row.append(listtabledata[listtableindex.index(index)])
+                else:
+                    row.append("")            
             for item in summary:
                 row.append(item)
             for item in tags:
